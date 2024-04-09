0689736435f17c1cfa64bcfb88b7e0e4192b014d
Saida <argelon1@gmail.com>
2024-04-09 10:48:55 +0300

    new workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,7 @@
-from pathlib import Path
-from dotenv import load_dotenv
-
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -66,10 +66,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-	'USER': os.getenv('POSTGRES_USER', 'django'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-	'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', 5432)
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
