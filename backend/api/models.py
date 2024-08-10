b313f3e0d89dd9e5c6d88ec23a4982305fd2847f
Saida <argelon1@gmail.com>
2024-08-10 23:13:49 +0300

    dd

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,5 +6,5 @@ class Task(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
-    def _str_(self):
+    def _str_(self):#htktkkt
         return self.title
